f32adb9c38997620999e8cda2a1aa0d5f83c8143
Galih Akbar <galihakbar.ga91@gmail.com>
2022-01-02 14:34:51 +0700

    Memperbaiki output

--- main.py
+++ main.py
@@ -6,24 +6,29 @@ from happy_golf import total_skor, rerata, baca_data_input
 # Interactive
 def main(pemain_golf):
     print_judul()
-    print_nama(pemain_golf)
-    print(pemain_golf)
 
     # Print data
+    jumlah_data = [len(data_pemain) for data_pemain in pemain_golf]
+    terpanjang = max(jumlah_data)
+    index_data_pemain_terpanjang = jumlah_data.index(terpanjang)
     data_table = {
-        'Holes': [hole for hole in list(pemain_golf[0])[1:]]
+        'Holes': [hole for hole in list(pemain_golf[index_data_pemain_terpanjang])[1:]]
     }
     for data_pemain in pemain_golf:
         data_table[data_pemain['nama']] = list(data_pemain.values())[1:]
 
     table(data_table)
+
+    # Pemenang
     pemain_golf = total_skor(pemain_golf)
+    print_pemenang(pemain_golf)
+
     print('\n\nDaftar Pemain')
     table({
         'Player Name': [list(pemain.keys())[0] for pemain in pemain_golf],
         'Score': [list(pemain.values())[0] for pemain in pemain_golf]
     })
-    print_pemenang(pemain_golf)
+
     print('\n\nRata - rata seluruh pemain golf:')
     print(rerata(pemain_golf))
 
--- table.py
+++ table.py
@@ -4,12 +4,10 @@ def table_sep(len_columns):
     print('+')
 
 
-def table_body(bodies, len_columns, justify):
-    for len_column, body, just in zip(len_columns, bodies, justify):
-        if just == 'right':
+def table_body(bodies, len_columns):
+    for len_column, body in zip(len_columns, bodies):
+        if isinstance(body, int):
             print('|', str(body).rjust(len_column), end=' ')
-        elif just == 'center':
-            print('|', str(body).center(len_column), end=' ')
         else:
             print('|', str(body).ljust(len_column), end=' ')
     print('|')
@@ -21,30 +19,8 @@ def table_header(headers, len_columns):
     print('|')
 
 
-# def table(pemain_golf, header1, header2):
-#     len_column_1 = max([len(list(pemain.keys())[0])
-#                        for pemain in pemain_golf]+[len(header1)])
-#     len_column_2 = max([len(str(list(pemain.values())[0]))
-#                        for pemain in pemain_golf]+[len(header2)])
-#     len_columns = [len_column_1, len_column_2]
-#     table_sep(len_columns)
-#     table_header(
-#         headers=[header1, header2],
-#         len_columns=len_columns
-#     )
-#     table_sep(len_columns)
-#     for pemain in pemain_golf:
-#         table_body(
-#             bodies=[list(pemain.keys())[0], str(list(pemain.values())[0])],
-#             len_columns=len_columns,
-#             justify=['left', 'right']
-#         )
-#     table_sep(len_columns)
-
-
 def table(kwargs):
     headers = list(kwargs)
-    len_kwargs = len(headers)
     len_columns = list()
     for k, v in kwargs.items():
         len_columns.append(max([len(str(item)) for item in v]+[len(str(k))]))
@@ -55,8 +31,11 @@ def table(kwargs):
     for idx in range(max([len(x) for x in kwargs.values()])):
         tmp = list()
         for header in headers:
-            tmp.append(kwargs[header][idx])
+            try:
+                tmp.append(kwargs[header][idx])
+            except IndexError:
+                tmp.append('')
         bodies.append(tmp)
     for item in bodies:
-        table_body(item, len_columns, ['left']*len_kwargs)
+        table_body(item, len_columns)
     table_sep(len_columns)
